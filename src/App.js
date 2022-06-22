324ca8c977abd3097c767ad3941d2b9fbf159101
mari293 <maricruzg020903@gmail.com>
2022-06-21 19:46:27 -0500

    Api marvel React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [buscar, setBuscar]= useState('')
+
+  const [resultado, setResultado]=useState('');
+
+  const xd = (nombre)=>{
+    setResultado(<div>
+      <h1>Que onda perro</h1>
+      <h2>el nombre es {nombre}</h2>
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+        <li>d</li>
+        <li>e</li>
+        <li>f</li>
+      </ul>
+    </div>);
+  }
+
+  const fetiches  = async () =>{
+    let data = {};
+    let nombre = '';
+    await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${buscar}&ts=1&apikey=8647391dcd64fb37aed65587c692cb85&hash=059c9891e0d7109faefe1dc8bfd6d1ff`)
+    .then(response=>response.json())
+    .then(data_f=>{data = data_f; console.log(data_f);})
+    nombre = data.data.results[0].name;
+    xd(nombre)
+  };
+
+  const cambiarNombre = (e) => {
+    if (e.target.value != ''){
+      setBuscar(document.getElementById('buscar').value)
+    fetiches();
+    }
+    else{
+      let output = document.getElementById('busqueda').innerHTML = '';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='nav'>
+        <h1>Tus personajes Favoritos</h1>
+      </nav>
+      <input type="text" id='buscar' onChange={cambiarNombre}></input>
+     <button onClick={fetiches}>data</button>
+      <button onClick={xd}>xd</button>
+     <div id='busqueda'>{resultado}</div>
     </div>
   );
 }
