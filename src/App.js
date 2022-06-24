23bd1b622a1c1899f3349bfab106673899ab53b6
Ethiem <maricruzg020903@gmail.com>
2022-06-23 21:36:16 -0500

    actualizacion 3

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react'
+import './App.css'
+import Personajes from './Personajes'
 
 function App() {
-  const [nombre, setNombre] = useState('');
-  const [descripcion, setDescripcion] = useState('');
-  const [buscar, setBuscar]= useState('')
+  const [vis, setVis] = useState('hidden')
 
-  const [resultado, setResultado]=useState('');
-
-  const xd = (nombre, imagen, descripcion)=>{
-    setResultado(<div>
-      <h1>Que onda perro</h1>
-      <h2>el nombre es {nombre}</h2>
-      <img src={imagen}></img>
-      <p>Descripcion: {descripcion}</p>
-    </div>);
-  }
-
-  const fetiches  = async () =>{
-    let data = {};
-    let nombre = '';
-    let imagen= '';
-    let descripcion = '';
-    await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${buscar}&ts=1&apikey=8647391dcd64fb37aed65587c692cb85&hash=059c9891e0d7109faefe1dc8bfd6d1ff`)
-    .then(response=>response.json())
-    .then(data_f=>{data = data_f; console.log(data_f);})
-    nombre = data.data.results[0].name;
-    imagen= data.data.results[0].thumbnail.path+'/standard_fantastic.'+data.data.results[0].thumbnail.extension;
-    descripcion = data.data.results[0].description;
-    xd(nombre, imagen, descripcion)
-  };
-
-  const cambiarNombre = (e) => {
-    if (e.target.value != ''){
-      setBuscar(document.getElementById('buscar').value)
-    fetiches();
-    }
-    else{
-      let output = document.getElementById('busqueda').innerHTML = '';
-    }
+  const Ver = () =>{
+    setVis('visible');
   }
 
   return (
-    <div className="App">
-      <nav className='nav'>
-        <h1>Tus personajes Favoritos</h1>
-      </nav>
-      <input type="text" id='buscar' onChange={cambiarNombre}></input>
-     <button onClick={fetiches}>data</button>
-      <button onClick={xd}>xd</button>
-     <div id='busqueda'>{resultado}</div>
-    </div>
-  );
+    <>
+      <div className='lainfo'>
+          <h1 className='Titulo'>The <i>Marvel</i>ious info</h1>
+          <nav className='nave'>
+            <button className='botonx' onClick={Ver}>Personajes</button>
+            <button className='botonx' onClick={Ver}>Comics</button>
+            <button className='botonx' onClick={Ver}>Eventos</button>
+            <button className='botonx' onClick={Ver}>Historias</button>
+          </nav>
+        </div>
+      
+      <Personajes show={vis}/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Personajes.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+function Personajes(props) {
+  
+  const [buscar, setBuscar]= useState('')
+  const [resultado, setResultado]=useState(<></>);
+
+  const xd = (nombre, imagen, descripcion)=>{
+    setResultado(<div>
+      <h1>Search for your Character</h1>
+      <h2>The Name is {nombre}</h2>
+      <div className='info'>
+        <p className='description'><h2>Description: </h2>{descripcion}</p>
+        <img src={imagen} alt="imagen de muestra del persoaje" className='character'></img>
+      </div>
+    </div>)
+  }
+
+  const fetiches  = async () =>{
+    let data = {};
+    let nombre = '';
+    let imagen= '';
+    let descripcion = '';
+    await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${buscar}&ts=1&apikey=8647391dcd64fb37aed65587c692cb85&hash=059c9891e0d7109faefe1dc8bfd6d1ff`)
+    .then(response=>response.json())
+    .then(data_f=>{data = data_f; console.log(data_f);})
+    let datos = data.data.results;
+    for(let i=0; i<datos.length; i++){
+      nombre = data.data.results[i].name;
+      imagen= data.data.results[i].thumbnail.path+'/detail.'+data.data.results[i].thumbnail.extension;
+      if(data.data.results[i].description==''){
+        descripcion = 'No description available'}
+      else{
+        descripcion = data.data.results[i].description;
+      }
+      xd(nombre, imagen, descripcion)
+    }
+  }; 
+
+  const cambiarNombre = (e) => {
+    if (e.target.value !== ''){
+      setBuscar(document.getElementById('buscar').value)
+    fetiches();
+    }
+    else{
+      document.getElementById('busqueda').innerHTML = '';
+    }
+  }
+
+  return (
+    <>
+    <div className="App" style={{visibility: props.show}}>
+      <nav className='nav'>
+        <h1 className='title' >Your favorite characters</h1>
+        <div>
+          
+        </div>
+      </nav>
+      <div className='apto'>
+        <input type="text" id='buscar' className='search' onChange={cambiarNombre}></input>
+      </div>
+     <div id='busqueda'>{resultado}</div>
+    </div>
+    </>
+     );
+}
+
+export default Personajes;
